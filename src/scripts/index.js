765764493600129774a3e568232f9cd40b517dff
coderanac <coder.anac@gmail.com>
2020-04-06 19:24:52 -0300

    :zap: Insert mask and result

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,38 @@
-import { numberCard } from './selectors.js';
+import { buttonValidateCard, inputNumberCard } from './selectors.js';
+import validator from './validator.js';
 
-numberCard.addEventListener('input', () => {
-    let valueCard = numberCard.value;
-    let mask = '';
-    for(let i = 4; i > valueCard.length; i+4) {
-        mask += valueCard.substr(0,4) + '.';
-    }
-})
+inputNumberCard.addEventListener('input', () => {
+  let valueCard = inputNumberCard.value;
+  inputNumberCard.value = maskCard(valueCard);
+});
+
+buttonValidateCard.addEventListener('click', () => {
+  let valueCard = inputNumberCard.value;
+  let numberCard = valueCard.replace(/\D/g, '')
+  let result = validator.valid(numberCard);
+  if(result) {
+    alert('valid');
+  } else {
+    alert('invalid');
+  }
+});
+
+function maskCard(valueCard) {
+  const separator = ' . ';
+  let numberCard = valueCard.replace(/\D/g, '');
+
+  if(numberCard.length > 4) {
+    numberCard = numberCard.substr(0, 4) + separator + numberCard.substr(4);
+  }
+
+  if(numberCard.length > 8) {
+    numberCard = numberCard.substr(0, 8 + separator.length) + separator + numberCard.substr(8 + separator.length);
+  }
+
+  if(numberCard.length > 12 + separator.length * 2) {
+    numberCard = numberCard.substr(0, 12 + separator.length * 2) + separator + numberCard.substr(12 + separator.length * 2, 4);
+  }
+
+  return numberCard;
+}
+
